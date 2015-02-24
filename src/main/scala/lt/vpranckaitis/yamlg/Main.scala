63e44812db8937484603430ce8f42723d51ad053
Vilius Pranckaitis <vpranckaitis@gmail.com>
2015-02-24 23:21:18 +0200

    Updated board representation to sparse matrix

--- src/main/scala/lt/vpranckaitis/yamlg/Main.scala
+++ src/main/scala/lt/vpranckaitis/yamlg/Main.scala
@@ -11,6 +11,24 @@ object Main {
   def main(args: Array[String]): Unit = {
     val webApiActor = system.actorOf(Props[WebApiActor])
     
-    IO(Http) ! Http.Bind.apply(webApiActor, interface = "0.0.0.0", port = 8080)
+    //IO(Http) ! Http.Bind.apply(webApiActor, interface = "0.0.0.0", port = 8080)
+    
+    Thread.sleep(500)
+    
+    val t1 = System.currentTimeMillis()
+    val b = Board("1111000011110000111100000000000000000000000022220000222200002222", '1', false)
+    //val b = Board("0011110001011000011110000000001000000000000022220000222200002222", '1', false)
+    
+    val filterDistance = b.distance + 2
+    val v1 = b.step(1, 5, filterDistance)
+    println(v1.size)
+    println((v1. map { _.toString.grouped(8) mkString "\n" }) mkString "\n\n")
+    val v2 = v1 flatMap { _.step(1, 5, filterDistance ) } 
+    println(v2.size)
+    val v3 = v2 flatMap { _.step(1, 5, filterDistance) }
+    println(v3.size)
+    val v4 = v3 flatMap { _.step(1, 5, filterDistance) }
+    println(v4.size)
+    println(System.currentTimeMillis() - t1)
   }
 }
--- src/main/scala/lt/vpranckaitis/yamlg/game/Board.scala
+++ src/main/scala/lt/vpranckaitis/yamlg/game/Board.scala
@@ -16,43 +16,76 @@ object Board {
   
   def distanceFunction: DistanceFunction = manhattanDistance
   
+  def distance(x: Int, y: Int) = distanceFunction(width, height)(x, y)
+  
   val minDistance = { 
     def dist = distanceFunction(0, 0)
     val everyDistance = for {
-      i <- 1 to piecesHeight; 
-      j <- 1 to piecesWidth
-    } yield dist(i, j)
+      y <- 1 to piecesHeight; 
+      x <- 1 to piecesWidth
+    } yield dist(x, y)
     
     everyDistance.reduce(_ + _)
   }
   
-  def apply(arrangement: String, side: Char, dir: Boolean) = new Board(arrangement, side, true)
+  def apply(arrangement: String, side: Char, dir: Boolean) = {
+    val grouped = arrangement.zipWithIndex groupBy { _._1 }
+    
+    def posToCoord(a: Int) = (a / width, a % width)
+    
+    val own = (grouped('1') map { t => posToCoord(t._2) }).toSet
+    val other = (grouped('2') map { t => posToCoord(t._2) }).toSet
+    new Board(own, other)
+  }
+  
   def isValid(arrangement: String) = {
     (arrangement.length == height * width) && 
       List(cpuSquare, playerSquare).forall(c => (arrangement.count(_ == c) == pieces))
   }
 }
 
-class Board(arrangement: String, side: Char, dir: Boolean) {
+class Board(val own: Set[(Int, Int)], val other: Set[(Int, Int)]) {
   import lt.vpranckaitis.yamlg.game.Board._
-  
-  private val board: Array[Array[Int]] = {
-    val mapping: Map[Char, Int] = Map(emptySquare -> 0, cpuSquare -> 1, playerSquare -> 2)
-    
-    def deserialize(s: String) = s map { mapping } toArray
-    
-    arrangement.grouped(Board.width) map { deserialize } toArray
-  }
   
   lazy val distance = {
     def dist = distanceFunction(Board.width, Board.height)
+    own map { xy => dist(xy._1, xy._2) } reduce { _ + _ }
+  }
+  
+  
+  
+  def move(x1: Int, y1: Int)(x2: Int, y2: Int) = {
+    new Board(own - Tuple2(x1, y1) + Tuple2(x2, y2), other)
+  }
+  
+  def step(p: Int, maxJumps: Int, filterDistance: Double) = {
+    def inside(x: Int, y: Int) = { x >= 0 && x < Board.width && y >= 0 && y < Board.height} 
     
-    val everyDistance = for {
-      i <- 0 until height; 
-      j <- 0 until width 
-      if board(i)(j) == 1
-    } yield dist(j, i)
-                                  
-    everyDistance.reduce(_ + _)
+    def neighbouringBoards(x: Int, y: Int) = {
+      val moves = List((1, 0), (0, 1), (-1, 0), (0, -1))
+      def moveFromCurrent: (Int, Int) => Board = move(x, y)
+      val stepAway = for {
+        (dx, dy) <- moves
+        if inside(x + dx, y + dy) && !own(x + dx, y + dy) && !own(x + dx, y + dy) && filterDistance > distance - Board.distance(x, y) + Board.distance(x + dx, y + dy)
+      } yield moveFromCurrent(x + dx, y + dy)
+      stepAway
+    }
+    
+    val cascadedBoards = for {
+      (x, y) <- own
+    } yield neighbouringBoards(x, y)
+    
+    cascadedBoards.flatten
+  }
+  
+  override def toString() = {
+    val chars = for {
+      x <- 0 until Board.width
+      y <- 0 until Board.height
+    } yield if (own(x, y)) '1' else if (other(x, y)) '2' else '0'
+    val strBuild = chars.foldLeft(new StringBuilder(Board.width * Board.height)) {
+      (acc: StringBuilder, b: Char) => acc.append(b)
+    }
+    strBuild.toString()
   }
 }
